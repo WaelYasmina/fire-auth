aa1665cf1f696cebdc2f3d3e1fbbd55aac3e5fa9
WaelYasmina <waelyasmina@outlook.com>
2019-07-20 19:00:02 +0100

    Sign in with phone number

--- public/js/home.js
+++ public/js/home.js
@@ -16,7 +16,8 @@ const phoneNumberField = document.getElementById('phoneNumber');
 const codeField = document.getElementById('code');
 const labels = document.getElementsByTagName('label');
 const signInWithMail = document.getElementById('signInWithMail');
-const signInWithPhone = document.getElementById('signInWithPhone');
+const signInWithPhoneButton = document.getElementById('signInWithPhone');
+const getCodeButton = document.getElementById('getCode');
 const signUp = document.getElementById('signUp');
 const failureModal = document.querySelector('.failure');
 const signInWithGoogleButton = document.getElementById('signInWithGoogle');
@@ -28,6 +29,37 @@ const signInWithFacebookButton = document.getElementById('signInWithFacebook');
 //than firebase.auth().signInWithEmailAndPassword
 //also it's less repetitive since we are using it more than once
 const auth = firebase.auth();
+
+window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('recaptcha-container');
+
+recaptchaVerifier.render().then(widgetId => {
+  window.recaptchaWidgetId = widgetId;
+})
+
+const sendVerificationCode = () => {
+  const phoneNumber = phoneNumberField.value;
+  const appVerifier = window.recaptchaVerifier;
+
+  auth.signInWithPhoneNumber(phoneNumber, appVerifier)
+  .then(confirmationResult => {
+    const sentCodeId = confirmationResult.verificationId;
+    signInWithPhoneButton.addEventListener('click', () => signInWithPhone(sentCodeId));
+  })
+}
+
+const signInWithPhone = sentCodeId => {
+  const code = codeField.value;
+  const credential = firebase.auth.PhoneAuthProvider.credential(sentCodeId, code);
+  auth.signInWithCredential(credential)
+  .then(() => {
+    window.location.assign('./profile');
+  })
+  .catch(error => {
+    console.error(error);
+  })
+}
+
+getCodeButton.addEventListener('click', sendVerificationCode);
 
 const signInWithFacebook = () => {
   const facebookProvider = new firebase.auth.FacebookAuthProvider();
