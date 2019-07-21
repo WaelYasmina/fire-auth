af7335badb6716fa9a291a3f0efefdf9fd25c427
WaelYasmina <waelyasmina@outlook.com>
2019-07-21 12:39:22 +0100

    update

--- public/js/home.js
+++ public/js/home.js
@@ -30,37 +30,6 @@ const signInWithFacebookButton = document.getElementById('signInWithFacebook');
 //also it's less repetitive since we are using it more than once
 const auth = firebase.auth();
 
-window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('recaptcha-container');
-
-recaptchaVerifier.render().then(widgetId => {
-  window.recaptchaWidgetId = widgetId;
-})
-
-const sendVerificationCode = () => {
-  const phoneNumber = phoneNumberField.value;
-  const appVerifier = window.recaptchaVerifier;
-
-  auth.signInWithPhoneNumber(phoneNumber, appVerifier)
-  .then(confirmationResult => {
-    const sentCodeId = confirmationResult.verificationId;
-    signInWithPhoneButton.addEventListener('click', () => signInWithPhone(sentCodeId));
-  })
-}
-
-const signInWithPhone = sentCodeId => {
-  const code = codeField.value;
-  const credential = firebase.auth.PhoneAuthProvider.credential(sentCodeId, code);
-  auth.signInWithCredential(credential)
-  .then(() => {
-    window.location.assign('./profile');
-  })
-  .catch(error => {
-    console.error(error);
-  })
-}
-
-getCodeButton.addEventListener('click', sendVerificationCode);
-
 const signInWithFacebook = () => {
   const facebookProvider = new firebase.auth.FacebookAuthProvider();
 
