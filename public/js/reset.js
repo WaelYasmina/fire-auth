3511a291235ad41148d94e30046b17d6c94740a1
WaelYasmina <waelyasmina@outlook.com>
2019-07-28 19:15:24 +0100

    Reset password

--- public/js/reset.js
+++ public/js/reset.js
@@ -4,6 +4,27 @@ const resetPassword = document.getElementById('resetPassword');
 const successModal = document.querySelector('.success');
 const failureModal = document.querySelector('.failure');
 
+const auth = firebase.auth();
+
+//auth.languageCode = 'DE_de';
+
+auth.useDeviceLanguage();
+
+const resetPasswordFunction = () => {
+    const email = mailField.value;
+
+    auth.sendPasswordResetEmail(email)
+    .then(() => {
+        console.log('Password Reset Email Sent Successfully!');
+    })
+    .catch(error => {
+        console.error(error);
+    })
+}
+
+
+resetPassword.addEventListener('click', resetPasswordFunction);
+
 //Animations
 mailField.addEventListener('focus', () => {
     labels.item(0).className = "focused-field";
