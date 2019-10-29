1ab23c818f2de2707f30cbfdd50a4c82e47b3807
WaelYasmina <waelyasmina@outlook.com>
2019-10-29 02:01:34 +0100

    Update home.js

--- public/js/home.js
+++ public/js/home.js
@@ -27,6 +27,18 @@ const signInWithGoogleButton = document.getElementById('signInWithGoogle');
 //also it's less repetitive since we are using it more than once
 const auth = firebase.auth();
 
+const signInWithGoogle = () => {
+  const googleProvider = new firebase.auth.GoogleAuthProvider();
+
+  auth.signInWithPopup(googleProvider)
+  .then(() => {
+    window.location.assign('./profile');
+  })
+  .catch(error => {
+    console.error(error);
+  })
+}
+
 signInWithGoogleButton.addEventListener('click', signInWithGoogle);
 
 //Sign in function (email and password authentication)
