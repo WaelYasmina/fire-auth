66f67938b4ade96d7e7240d33f58f870f88ed18d
sori <smcdidcksthf@naver.com>
2022-05-05 21:02:20 +0900

    addDashboard

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,37 +1,42 @@
-const logOut = document.getElementById('logOut');
-const mergeAccounts = document.getElementById('mergeAccounts');
-const modifyAccount = document.getElementById('modifyAccount');
-const displayNameHolder = document.getElementById('displayNameHolder');
-const photoHolder = document.getElementById('photoHolder');
+const logOut = document.getElementById("logOut");
+const mergeAccounts = document.getElementById("mergeAccounts");
+const modifyAccount = document.getElementById("modifyAccount");
+const displayNameHolder = document.getElementById("displayNameHolder");
+const photoHolder = document.getElementById("photoHolder");
+const goToDashboard = document.getElementById("goToDashboard");
 
 const auth = firebase.auth();
 
-logOut.addEventListener('click', () => {
-    //signOut() is a built in firebase function responsible for signing a user out
-    auth.signOut()
+logOut.addEventListener("click", () => {
+  //signOut() is a built in firebase function responsible for signing a user out
+  auth
+    .signOut()
     .then(() => {
-        window.location.assign('../');
+      window.location.assign("../");
     })
-    .catch(error => {
-        console.error(error);
-    })
-})
+    .catch((error) => {
+      console.error(error);
+    });
+});
 
-auth.onAuthStateChanged(user => {
-    console.log(user);
-    //display the displayName and photoURL of the user on the page
-    if(user.displayName)
-        displayNameHolder.innerText = user.displayName;
-    if(user.photoURL)
-        photoHolder.setAttribute('src', user.photoURL);
-})
+auth.onAuthStateChanged((user) => {
+  console.log(user);
+  //display the displayName and photoURL of the user on the page
+  if (user.displayName) displayNameHolder.innerText = user.displayName;
+  if (user.photoURL) photoHolder.setAttribute("src", user.photoURL);
+});
 
 //Go to modification page
-modifyAccount.addEventListener('click', () => {
-    window.location.assign('../edit');
+modifyAccount.addEventListener("click", () => {
+  window.location.assign("../edit");
 });
 
 //Go to merge accounts page
-mergeAccounts.addEventListener('click', () => {
-    window.location.assign('../merge');
+mergeAccounts.addEventListener("click", () => {
+  window.location.assign("../merge");
+});
+
+//Go to Dashboard page
+goToDashboard.addEventListener("click", () => {
+  window.location.assign("../dashboard");
 });
